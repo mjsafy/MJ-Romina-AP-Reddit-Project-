3cb6a9674378bd36bd0aa65a4b458ac32d764510
MohamadJavad <mjs.10om1382@gmail.com>
2022-05-23 16:01:58 +0430

    add text fields to signup screen

--- lib/SignUp.dart
+++ lib/SignUp.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -8,6 +9,26 @@ class SignUp extends StatefulWidget {
 }
 
 class _SignUpState extends State<SignUp> {
+  TextEditingController userNameC;
+  TextEditingController passwordC;
+  TextEditingController emailC;
+
+  @override
+  void initState() {
+    userNameC = TextEditingController();
+    passwordC = TextEditingController();
+    emailC = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    userNameC.dispose();
+    passwordC.dispose();
+    emailC.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +38,52 @@ class _SignUpState extends State<SignUp> {
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.center,
           mainAxisAlignment: MainAxisAlignment.center,
-
+          children: [
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 4),
+              child: TextField(
+                  controller: userNameC,
+                  decoration: const InputDecoration(
+                      fillColor: Colors.tealAccent,
+                      filled: true,
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      hintText: "userName")),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 4),
+              child: TextField(
+                  controller: passwordC,
+                  decoration: const InputDecoration(
+                      fillColor: Colors.tealAccent,
+                      filled: true,
+                      contentPadding:
+                          EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.transparent),
+                          borderRadius: BorderRadius.all(Radius.circular(40))),
+                      hintText: "Password")),
+            ),
+            Container(
+              padding: EdgeInsets.symmetric(vertical: 4),
+              child: TextField(
+                controller: emailC,
+                decoration: const InputDecoration(
+                    fillColor: Colors.tealAccent,
+                    filled: true,
+                    contentPadding:
+                        EdgeInsets.symmetric(vertical: 12, horizontal: 8),
+                    enabledBorder: OutlineInputBorder(
+                        borderSide: BorderSide(color: Colors.transparent),
+                        borderRadius: BorderRadius.all(Radius.circular(40))),
+                    hintText: "Email",
+                    prefixIcon: Icon(Icons.email)),
+              ),
+            ),
+          ],
         ),
       ),
     );

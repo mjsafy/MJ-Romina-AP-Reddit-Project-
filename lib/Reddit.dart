1fa8db01fb10ccfadfbab8decd4c01ac0616b4e3
MohamadJavad <mjs.10om1382@gmail.com>
2022-05-23 15:21:19 +0430

    create first page

---
+++ lib/Reddit.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+
+class Reddit extends StatelessWidget {
+  const Reddit({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
+
+    return Scaffold(
+      appBar: AppBar(
+          centerTitle: true,
+          title: Text(
+            "Reddit",
+            style: TextStyle(fontSize: 25),
+          )),
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [
+                  Colors.purple,
+                  Colors.black,
+                  Colors.black,
+                  Colors.purple,
+                ])),
+        child: Container(
+          alignment: Alignment.center,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                  margin: EdgeInsets.only(bottom: h / 30),
+                  width: w / 2,
+                  height: h / 10,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(40)),
+                      gradient: LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          colors: [
+                            Colors.deepPurple,
+                            Colors.deepOrange,
+                          ])),
+                  child: TextButton(
+                      autofocus: true,
+                      child: const Text(
+                        "Sign Up",
+                        style: TextStyle(fontSize: 25, color: Colors.white),
+                      ))),
+              Container(
+                  width: w / 2,
+                  height: h / 10,
+                  decoration: const BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(40)),
+                      gradient: LinearGradient(
+                          begin: Alignment.topRight,
+                          end: Alignment.bottomLeft,
+                          colors: [
+                            Colors.deepOrange,
+                            Colors.deepPurple,
+                          ])),
+                  child: TextButton(
+                      autofocus: true,
+                      child: Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white, fontSize: 25),
+                      ))),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}

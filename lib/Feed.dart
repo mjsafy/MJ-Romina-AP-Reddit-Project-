3e2ddc796ebf7d56b9dd46c0f27580442b25d7be
Romina Shekary <96865608+RominaShekary@users.noreply.github.com>
2022-05-28 20:15:05 +0430

    feed page and postWidget

--- lib/Feed.dart
+++ lib/Feed.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+
+import 'Post.dart';
 class Feed extends StatefulWidget {
   @override
   State<Feed> createState() => _FeedState();
@@ -7,12 +9,66 @@ class Feed extends StatefulWidget {
 class _FeedState extends State<Feed> {
   @override
   Widget build(BuildContext context) {
-    return Text (
-      'Feed',
-      style: TextStyle(
-        fontSize: 20.0,
-        fontWeight: FontWeight.bold,
-      ),
-    );
+    return ListView.builder(
+        itemCount: 5,
+        itemBuilder: (context, index) {
+      return postWidget();
+    });
   }
 }
+
+class postWidget extends StatefulWidget {
+  Post post;
+  postWidget({this.post});
+  @override
+  State<postWidget> createState() => _postWidgetState();
+}
+
+class _postWidgetState extends State<postWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              Container(
+                margin: EdgeInsets.only(left: 10.0, top: 10.0),
+                child: const CircleAvatar(
+                  backgroundImage: NetworkImage('https://quera.org/media/CACHE/images/public/avatars/7f6c9d7981bb4785b2d1fe571e574f43/93aff44bde76be50be9980c28a16bc46.jpg'),
+                  radius: 25.0,
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.only(left: 10.0, top: 10.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Text(
+                      'John Doe',
+                      style: TextStyle(
+                        fontSize: 15.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    Text(
+                      '@johndoe',
+                      style: TextStyle(
+                        fontSize: 12.0,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+          Container(
+            margin: EdgeInsets.only(left: 10.0, top: 10.0),
+            child: Text(widget.post.content),
+          ),
+        ],
+      ),);
+  }
+}
+

e16cfa749912a95e1e19de20d26104c6894e2e44
MohamadJavad <mjs.10om1382@gmail.com>
2022-07-01 23:54:06 +0430

    add getUser

--- lib/Convertor.dart
+++ lib/Convertor.dart
@@ -2,5 +2,12 @@ import 'package:reddit/Post.dart';
 import 'package:reddit/User.dart';
 
 class Convertor{
-
+  //uname:password-email
+  static User getUser(String command){
+    List<String> split = command.split(":");
+    String uname = split.elementAt(0);
+    String password = split.elementAt(1).split("-").elementAt(0);
+    String email = split.elementAt(1).split("-").elementAt(1);
+    return User(username: uname , password: password , email: email);
+  }
 }
